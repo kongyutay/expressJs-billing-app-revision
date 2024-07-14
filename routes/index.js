22716824d5ac16f05d88f60af9a3fb2b1fb4f423
Tay Kong Yu <kongyutay@gmail.com>
2024-07-14 12:15:25 +0800

    low db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '../data/db.json')
+const db = low(adapter)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/account', function(req, res, next) {
+  res.render('list');
+});
+
+router.post('/account', function(req, res, next) {
+  res.send('添加记录');
+  console.log(req.body)
 });
 
 router.get('/account/create', function(req, res, next) {
