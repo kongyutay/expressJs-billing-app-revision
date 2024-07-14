ae21c5557105194fa0d18d9caa682793408976c1
Tay Kong Yu <kongyutay@gmail.com>
2024-07-14 15:16:31 +0800

    basic crud with low db

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync(__dirname + '../data/db.json')
+const adapter = new FileSync(__dirname + '/../data/db.json')
 const db = low(adapter)
+const shortid = require('shortid')
 
 /* GET home page. */
 router.get('/account', function(req, res, next) {
-  res.render('list');
+  let accounts = db.get('accounts').value()
+  res.render('list', {accounts: accounts});
 });
 
 router.post('/account', function(req, res, next) {
-  res.send('添加记录');
-  console.log(req.body)
+  let id = shortid.generate()
+  db.get('accounts').unshift({id:id, ...req.body}).write();
+  res.render('success', {msg: '添加成功', url: '/account'});
 });
 
 router.get('/account/create', function(req, res, next) {
@@ -21,6 +24,12 @@ router.get('/account/create', function(req, res, next) {
 
 router.get('/account/list', function(req, res, next) {
   res.render('list', { title: 'Express' });
+});
+
+router.get('/account/:id', function(req, res, next) {
+  let id = req.params.id;
+  db.get('accounts').remove({id:id}).write()
+  res.render('success', {msg: '删除成功', url: '/account'});
 });
 
 module.exports = router;
