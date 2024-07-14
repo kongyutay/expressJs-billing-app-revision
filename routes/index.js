0fe2f08ec5fffa7523d17dc9c757be183cea09d4
Tay Kong Yu <kongyutay@gmail.com>
2024-07-14 18:37:03 +0800

    basic router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/account', function(req, res, next) {
+  res.render('list');
+});
+
+router.post('/account', function(req, res, next) {
+  //处理表单逻辑
+  console.log(req.body)
+  res.render('success', { msg: '添加成功', url: '/account' });
+});
+
+router.get('/account/create', function(req, res, next) {
+  res.render('create');
+});
+
 module.exports = router;
